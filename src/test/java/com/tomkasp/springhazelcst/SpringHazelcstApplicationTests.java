3c2b7c8835d78954a411dddbc2d85346079570f9
tomkasp@gmail.com <tomkasp@gmail.com>
2017-10-14 14:37:15 +0200

    Initial commit

---
+++ src/test/java/com/tomkasp/springhazelcst/SpringHazelcstApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tomkasp.springhazelcst;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringHazelcstApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
